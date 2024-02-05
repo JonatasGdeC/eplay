0eba98b9653358d97960f0d8d7b4352096dfd959
Jonatas <jonatas.07022003@gmail.com>
2024-02-05 14:23:11 -0300

    Criando o header e banner da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { GlobalCss } from './styles'
+import Header from './components/Header'
+import Banner from './components/Banner'
+
 function App() {
-  return <></>
+  return (
+    <>
+      <GlobalCss />
+      <div className="container">
+        <Header />
+      </div>
+      <Banner />
+    </>
+  )
 }
 
 export default App
