a999f9c79dc1a1d8144103d9e886039ef1664997
Jonatas <jonatas.07022003@gmail.com>
2024-02-21 14:45:50 -0300

    Criando o checkout de compra com formulário e forma de pagamento

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,38 +7,61 @@ import carrinho from '../../assets/images/carrinho.svg'
 import { open } from '../../store/reducers/cart'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
+import { useState } from 'react'
 
 const Header = () => {
   const dispatch = useDispatch()
   const { items } = useSelector((state: RootReducer) => state.cart)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
   }
   return (
     <S.HeaderBar>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="Eplay" />
-        </Link>
-        <nav>
-          <S.Links>
-            <S.LinkItem>
-              <Link to="/categorias">Categorias</Link>
-            </S.LinkItem>
-            <S.LinkItem>
-              <a href="#">Novidades</a>
-            </S.LinkItem>
-            <S.LinkItem>
-              <a href="#">Promoções</a>
-            </S.LinkItem>
-          </S.Links>
-        </nav>
-      </div>
-      <S.CardButton href="#" onClick={openCart}>
-        {items.length} - produto(s)
-        <img src={carrinho} alt="Carrinho de compras" />
-      </S.CardButton>
+      <S.HeaderRow>
+        <div>
+          <S.Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </S.Hamburguer>
+          <Link to="/">
+            <img src={logo} alt="Eplay" />
+          </Link>
+          <nav>
+            <S.Links>
+              <S.LinkItem>
+                <Link to="/categorias">Categorias</Link>
+              </S.LinkItem>
+              <S.LinkItem>
+                <a href="#">Novidades</a>
+              </S.LinkItem>
+              <S.LinkItem>
+                <a href="#">Promoções</a>
+              </S.LinkItem>
+            </S.Links>
+          </nav>
+        </div>
+        <S.CardButton href="#" onClick={openCart}>
+          {items.length}
+          <span> - produto(s)</span>
+          <img src={carrinho} alt="Carrinho de compras" />
+        </S.CardButton>
+      </S.HeaderRow>
+      <S.NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <S.Links>
+          <S.LinkItem>
+            <Link to="/categorias">Categorias</Link>
+          </S.LinkItem>
+          <S.LinkItem>
+            <a href="#">Novidades</a>
+          </S.LinkItem>
+          <S.LinkItem>
+            <a href="#">Promoções</a>
+          </S.LinkItem>
+        </S.Links>
+      </S.NavMobile>
     </S.HeaderBar>
   )
 }
